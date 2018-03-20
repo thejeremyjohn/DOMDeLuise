00520fd7a6f94a7f15329c4cb0b25a2e710c007c
Jeremy <thejeremyjohn@gmail.com>
2018-03-20 09:59:14 -0400

    dom deluise...get it?

--- DOMDeLuise/lib/dom_node_collection.js
+++ DOMDeLuise/lib/dom_node_collection.js
@@ -1,118 +1,118 @@
 class DOMNodeCollection {
-  constructor(html_array) {
-    this.html_array = html_array;
+  constructor(htmlArray) {
+    this.htmlArray = htmlArray;
   }
-  
+
   html(string) {
     if (typeof string === "string") {
-      this.html_array.forEach(function(el) {
+      this.htmlArray.forEach(function(el) {
         el.innerHTML = string;
       });
     } else {
-      return this.html_array[0].innerHTML;
+      return this.htmlArray[0].innerHTML;
     }
   }
-  
+
   empty() {
     this.html("");
   }
-  
+
   append(arg) {
     if (arg instanceof DOMNodeCollection) {
       const that = this;
-      arg.html_array.forEach(function (htmlel) {
+      arg.htmlArray.forEach(function (htmlel) {
         that.append(htmlel);
       });
     } else if (arg instanceof HTMLElement) {
-      this.html_array.forEach(function(el) {
+      this.htmlArray.forEach(function(el) {
         el.innerHTML += arg.outerHTML;
       });
-      
-      
+
+
     } else if (typeof arg === 'string') {
-      this.html_array.forEach(function(el) {
+      this.htmlArray.forEach(function(el) {
         el.innerHTML += arg;
       });
     }
   }
-  
+
   attr(key, val) {
     if (!val) {
-      return this.html_array[0].getAttribute(key);
+      return this.htmlArray[0].getAttribute(key);
     } else {
-      this.html_array.forEach(function(el) {
+      this.htmlArray.forEach(function(el) {
         el.setAttribute(key, val);
       });
     }
   }
-  
+
   addClass(className) {
-    this.html_array.forEach(function (htmlel) {
+    this.htmlArray.forEach(function (htmlel) {
       htmlel.classList.add(className);
     });
   }
-  
+
   removeClass(className) {
-    this.html_array.forEach(function (htmlel) {
+    this.htmlArray.forEach(function (htmlel) {
       htmlel.classList.remove(className);
     });
   }
-    
+
   children() {
-    const childs = this.html_array.map( (el) => {
+    const childs = this.htmlArray.map( (el) => {
       return Array.prototype.slice.call( el.children );
     });
     return new DOMNodeCollection(childs[0]);
   }
-  
+
   parent() {
     const par = [];
-    this.html_array.forEach( (el) => {
+    this.htmlArray.forEach( (el) => {
       if (!par.includes(el.parentNode)) {
          par.push(el.parentNode);
-      }  
+      }
     });
     return new DOMNodeCollection(par);
   }
-  
+
   find(selector) {
     let arr = [];
-    this.html_array.forEach( (el) => { 
+    this.htmlArray.forEach( (el) => {
       arr = arr.concat(
         Array.from(el.querySelectorAll(selector))
       );
     });
     return new DOMNodeCollection(arr);
   }
-  
+
   remove() {
-    this.html_array.forEach ( (el) => {
+    this.htmlArray.forEach ( (el) => {
       const parent = el.parentNode;
       parent.removeChild(el);
     });
-    this.html_array = [];
+    this.htmlArray = [];
   }
-  
+
   on (type, handler) {
-    this.html_array.forEach ( (el) => {
+    this.htmlArray.forEach ( (el) => {
       el.addEventListener(type, handler);
       el.type = type;
       el.listener = handler;
     });
   }
-    
+
   off (type, handler) {
-    this.html_array.forEach ( (el) => {
+    this.htmlArray.forEach ( (el) => {
       type = el.type || type;
       handler = el.listener || handler;
       el.removeEventListener(type, handler);
     });
   }
-  
+
   ready () {
-    
+
   }
-  
+
 }
 
 module.exports = DOMNodeCollection;
